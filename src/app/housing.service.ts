f989f5caf481f653d992f53c67d652a4a245d145
vedavathi-b26 <149676696+vedavathi-b26@users.noreply.github.com>
2023-11-15 15:51:37 +0530

    Update housing.service.ts

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
-
+import { SampleInterface } from './sample.interface/sampleinterface';
 @Injectable({
   providedIn: 'root'
 })
 export class HousingService {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
-  constructor() { }
+      protected sampleInterfaceList: SampleInterface[]= [
+        {
+          id: 0,
+          name: 'Acme Fresh Start Housing',
+          city: 'Chicago',
+          state: 'IL',
+          photo: `${this.baseUrl}/bernard-hermant-CLKGGwIBTaY-unsplash.jpg`
+        },
+        {
+          id: 1,
+          name: 'A113 Transitional Housing',
+          city: 'Santa Monica',
+          state: 'CA',
+          photo: `${this.baseUrl}/brandon-griggs-wR11KBaB86U-unsplash.jpg`
+        },
+        {
+          id: 2,
+          name: 'Warm Beds Housing Support',
+          city: 'Juneau',
+          state: 'AK',
+          photo: `${this.baseUrl}/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg`
+        }
+      ];
+
+      getAllSampleInterfaces(): SampleInterface[] {
+        return this.sampleInterfaceList;
+      }
+      getSampleInterfaceById(id: number): SampleInterface | undefined{
+        return this.sampleInterfaceList.find(sampleInterface => sampleInterface.id === id);
+      }
 }
