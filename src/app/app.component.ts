41976edb1b6ce0b2eb8a7c5a17b779538f5fb323
vedavathi-b26 <149676696+vedavathi-b26@users.noreply.github.com>
2023-11-15 19:49:43 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [HomeComponent,
+  RouterModule,
+],
   /*template: `<h1>Hello world!</h1>`, */
   template: `
     <main>
+      <a [routerLink]="['/']">
       <header class="brand-name">
         <img class="brand-logo" src="/assets/Veda.png" alt="logo" aria-hidden="true">
       </header>
+      </a>
       <section class="content">
-        <app-home></app-home>
+        <!-- <app-home></app-home> -->
+        <router-outlet></router-outlet>
       </section>
     </main>
   `,
