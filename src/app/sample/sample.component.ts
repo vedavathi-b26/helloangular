4b6eeac26be3d4feb71c98fcaa3a995d6af61935
vedavathi-b26 <149676696+vedavathi-b26@users.noreply.github.com>
2023-11-16 15:50:16 +0530

    Update sample.component.ts

--- src/app/sample/sample.component.ts
+++ src/app/sample/sample.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core'; /*related add Input parameter to the component*/
 import { CommonModule } from '@angular/common';
 import {SampleInterface } from '../sample.interface/sampleinterface';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
@@ -8,16 +9,18 @@ import {SampleInterface } from '../sample.interface/sampleinterface';
   standalone: true,
   imports: [
     CommonModule,
+    RouterModule
   ],
   template: `
   <section class="result">
         <img src="/assets/angular.jpg" alt="">
         <p> Second Component here </p>
-  </section>
+  </section><br>
   <section class="listing">
     <img class="listing-photo" [src]="sampleInterface.photo" alt="Exterior photo of {{sampleInterface.name}}">
     <h2 class="listing-heading">{{ sampleInterface.name }}</h2>
     <p class="listing-location">{{ sampleInterface.city}}, {{sampleInterface.state }}</p>
+    <a [routerLink]="['/details',sampleInterface.id]">LearnMore</a>
   </section> `,  /* Add interpolation to a component to a template*/
   
   styleUrls: ['./sample.component.css'],
