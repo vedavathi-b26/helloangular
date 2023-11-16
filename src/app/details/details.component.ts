1d93375132abe4dac706ddf75d1300e5153f580b
vedavathi-b26 <149676696+vedavathi-b26@users.noreply.github.com>
2023-11-16 15:51:12 +0530

    Update details.component.ts

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import {SampleInterface } from '../sample.interface/sampleinterface';
 
 @Component({
   selector: 'app-details',
+  standalone: true,
+  imports: [CommonModule],
   template: `
-    <p>
-      details works!
-    </p>
+    <article>
+      <img class="listing-photo" [src]="sampleInterface?.photo" alt="Exterior photo of {{ sampleInterface?.name}}"/>
+      <section class="listing-description">
+        <h2 class="listing-heading">{{sampleInterface?.name}}</h2>
+        <p class="listing-location">{{sampleInterface?.city}}, {{sampleInterface?.state}}</p>
+      </section>
+      <section class="listing-features">
+        <h2 class="section-heading">About this housing location</h2>
+        <ul>
+          <li>Units Available: {{sampleInterface?.availableUnits}}</li>
+        </ul>
+      </section>
+    </article>
   `,
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
 
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService = inject(HousingService);
+  sampleInterface: SampleInterface | undefined;
+  constructor(){
+    const sampleInterfaceId = Number(this.route.snapshot.params['id']);
+    this.sampleInterface = this.housingService.getSampleInterfaceById(sampleInterfaceId);
+  }
 }
