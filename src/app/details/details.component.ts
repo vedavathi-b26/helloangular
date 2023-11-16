6b10e647d43375b0486243b8ccfb72b100cd5683
vedavathi-b26 <149676696+vedavathi-b26@users.noreply.github.com>
2023-11-16 18:37:09 +0530

    Update details.component.ts

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import {SampleInterface } from '../sample.interface/sampleinterface';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,
+  ReactiveFormsModule],
   template: `
     <article>
       <img class="listing-photo" [src]="sampleInterface?.photo" alt="Exterior photo of {{ sampleInterface?.name}}"/>
@@ -21,6 +23,21 @@ import {SampleInterface } from '../sample.interface/sampleinterface';
           <li>Units Available: {{sampleInterface?.availableUnits}}</li>
         </ul>
       </section>
+      <section class="listing-apply">
+        <h2 class="section-heading">Apply Now to live here</h2>
+        <form [formGroup]="applyForm" (submit)="submitApplication()">
+        <label for="first-name">First Name</label>
+        <input id="first-name" type="text" formControlName="firstName">
+
+        <label for="last-name">Last Name</label>
+        <input id="last-name" type="text" formControlName="lastName">
+
+        <label for="email">Email</label>
+        <input id="email" type="email" formControlName="email">
+
+        <button type="submit" class="primary">Apply now</button>
+      </form>
+      </section>
     </article>
   `,
   styleUrls: ['./details.component.css']
@@ -30,8 +47,22 @@ export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
   sampleInterface: SampleInterface | undefined;
+
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  });
   constructor(){
     const sampleInterfaceId = Number(this.route.snapshot.params['id']);
     this.sampleInterface = this.housingService.getSampleInterfaceById(sampleInterfaceId);
+  }
+
+  submitApplication(){
+    this.housingService.submitApplication(
+    this.applyForm.value.firstName ??'',
+    this.applyForm.value.lastName ??'',
+    this.applyForm.value.email ??''
+    )
   }
 }
